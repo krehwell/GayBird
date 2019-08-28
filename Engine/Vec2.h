9bfce8192f90dd1eac319c3c5e3c505146cd2d7a
pikel <fahrimuham001@gmail.com>
2019-08-28 11:56:46 +0800

    added Vector class (2D)

---
+++ Engine/Vec2.h
@@ -0,0 +1,15 @@
+#pragma once
+class Vec2
+{
+public:
+	Vec2() = default;
+	Vec2(float x, float y);
+	Vec2 operator+(Vec2& rhs);
+	Vec2& operator+=(Vec2& rhs);
+	Vec2 operator*(float rhs);
+	Vec2& operator*=(float rhs);
+
+private:
+	float x;
+	float y;
+};
